f1704dbdea1e23cd64cab38a808a8b0dd3c852c4
maier_sebastian <sebastian.maier03@htl-salzburg.ac.at>
2021-12-07 15:53:08 +0100

    Sebi second bush

--- Wettbuero.cpp
+++ Wettbuero.cpp
@@ -2,8 +2,6 @@
 by Sebastian Maier and Leon Hipf
 */
 #include <fstream>
-#include <sstream>
-#include <vector>
 #include <iostream>
 #include <string>
 #include <cstring>
@@ -11,24 +9,47 @@ using namespace std;
 
 int main()
 {
+    //declare variables
     const int SIZE = 500;
-    char list[SIZE]; //array for the input file
-    string filename = "Noten.txt";
-    ifstream file;
+    char list_noten[SIZE];                 //array for the noten file
+    char list_wetten[SIZE];                //array for the wetten file
+    string filename_noten = "Noten.txt";   //noten filename
+    string filename_wetten = "Wetten.txt"; //wetten filename
+    ifstream file_noten;                   //storage for file_noten
+    ifstream file_wetten;                  //storage for file_wetten
 
-    file.open(filename.c_str()); //open input file
-    if (file.is_open())          //if the input file is open
+    /////////////// read the noten file //////////////////
+    file_noten.open(filename_noten.c_str()); //open input file
+    if (file_noten.is_open())                //if the input file is open
     {
-        while (!file.eof()) //eof = end of file
+        while (!file_noten.eof()) //eof = end of file
         {
-            file >> list;                     //write the file in the array
-            char *namen = strtok(list, ":");  //split at : -> print names
-            char *noten = strtok(NULL, "\n"); //split at \n -> print noten
-            cout << noten << endl;
+            file_noten >> list_noten;              //write the file in the array
+            char *namen = strtok(list_noten, ":"); //split at : -> print names
+            char *noten = strtok(NULL, "\n");      //split at \n -> print noten
+            cout << "Noten: " << noten << endl;
         }
     }
     else //Error message
         cerr
-            << "Can't find input file " << filename << endl;
-    file.close(); // Close input file
+            << "Can't find noten file " << filename_noten << endl;
+    file_noten.close(); // Close input file
+
+    /////////////// read the wetten file //////////////////
+    file_wetten.open(filename_wetten.c_str()); //open input file
+    if (file_wetten.is_open())                 //if the input file is open
+    {
+        while (!file_wetten.eof()) //eof = end of file
+        {
+            file_wetten >> list_wetten;             //write the file in the array
+            char *namen = strtok(list_wetten, ":"); //split at : -> print names
+            char *wetten = strtok(NULL, ",");       //split at , -> print noten
+            char *einsatz = strtok(NULL, "\n");     //split at \n -> print einsatz
+            cout << "Einsatz: " << einsatz << endl;
+        }
+    }
+    else //Error message
+        cerr
+            << "Can't find wetten file " << filename_wetten << endl;
+    file_wetten.close(); // Close input file
 }
