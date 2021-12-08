e88d1336fd5da244b93516df212c30c6ba14e4df
gitHipL <leon@NBE-HipL.localdomain>
2021-12-08 22:59:24 +0100

    get Testname

--- fill_bets.cpp
+++ fill_bets.cpp
@@ -1,6 +1,10 @@
 #include <fstream>
 #include <string>
 #include <cstring>
+#include <iostream>
+#include <ostream>
+#include <istream>
+#include <cstdio>
 
 using namespace std;
 
@@ -14,7 +18,13 @@ int main()
     ifstream file_bets;                    // storage for file_noten
     ifstream file_grades;                  // storage for file_wetten
 
+    string Testname;
+    bool fileexist = false;
+
     // Einlesen "Name Test"
+    cout << "Geben sie den Namen des Tests ein um den gewettet wird." << endl;
+    cin >> Testname;
+    cout << "Testname:" << Testname << endl;
 
     // if "Name Test"_bets.txt ist nicht da
     // bool exist = false;
